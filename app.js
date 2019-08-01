3d8795f0edca73382edd9ada1245bb8835699d3e
GOD <vtlvldmrvch@gmail.com>
2019-08-01 20:55:52 +0300

    added get users route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var db = require("./database");
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../database.js");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sql = "select * from user";
+  var params = [];
+  db.serialize(function() {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error": err.message});
+        return;
+      } else {
+        res.json({"message": "success", "data": rows})
+      }
+    });
+  })
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
